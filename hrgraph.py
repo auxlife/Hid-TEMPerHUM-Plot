146cdef50b48865fd34176f1bbcd15862d493ce6
auxlife <theju@auxlife.com>
2018-04-27 11:28:03 -0700

    Add files via upload

---
+++ hrgraph.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import MySQLdb as mdb
+import matplotlib
+import numpy as np
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+
+tempdata = []
+humdata = []
+dates = []
+# Read the data from db
+con = mdb.connectcon = ('DBHOST', 'DBUSER', 'DMPASS', 'DB');
+cur = con.cursor()
+cur.execute("SELECT datetime, temp, hum  FROM readings ORDER BY `datetime` DESC limit 120")
+for i in range(cur.rowcount):
+    row = cur.fetchone()
+    date = matplotlib.dates.date2num(row[0])
+    dates.append(date)
+    tempdata.append(row[1])
+	humdata.append(row[2])
+con.close()
+
+fig, ax = plt.subplots(figsize=(6,5))
+ax2 = ax.twinx()
+ax.plot_date(dates, tempdata,fmt='-')
+ax2.plot_date(dates, humdata,'r-')
+temp_mean = [np.mean(tempdata)]*len(dates)
+ax.plot(dates,temp_mean, linestyle='--')
+ax.xaxis.set_major_formatter(DateFormatter('%H:%M'))
+
+ax.set_ylabel('Temperature F')
+ax2.set_ylabel('RH %')
+for label in ax.get_xticklabels():
+    label.set_rotation(60)
+plt.tight_layout()
+
+plt.savefig('/var/www/html/hrtemp.png')
